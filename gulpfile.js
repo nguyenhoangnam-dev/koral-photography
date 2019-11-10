bb5d082e3316021bfe81440a6bc6d845d049161e
H Nam <congviec18120062@gmail.com>
2019-11-10 08:10:03 +0700

    v0.1.0: add struture for sass folder, sourcemap, fibers, html replace

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const gulp = require('gulp');
 const {series, parallel, src, dest} = require('gulp');
 const sass = require('gulp-sass');
@@ -11,22 +10,22 @@ const autoprefixer = require('gulp-autoprefixer');
 const csso = require('gulp-csso');
 const htmlmin = require('gulp-htmlmin');
 const clean = require('gulp-clean');
-const size = require('gulp-filesize');
+const size = require('gulp-size');
+const htmlReplace = require('gulp-html-replace');
+const sourcemaps = require('gulp-sourcemaps');
+const newer = require('gulp-newer');
+const Fiber = require('fibers');
 
 function style() {
-  // 1. where is my sss file
-  return src('./src/scss/**/*.scss')
-    // 2. pass that file through compiler
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer())
-    // Minify css
-    .pipe(csso())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    // 3. where do i save the complete css
+  return src('./src/scss/*.scss')
+    .pipe(sourcemaps.init())
+      .pipe(sass({
+        fiber           : Fiber,
+        precision       : 3,
+        errLogToConsole : true
+      }).on('error', sass.logError))
+    .pipe(sourcemaps.write('.'))
     .pipe(dest('./src/css'))
-  // 4. stream changes to all browser
     .pipe(browserSync.stream());
 }
 
@@ -43,14 +42,23 @@ function watch() {
 
 function minifyImage() {
   return src(['./src/img/*', '!./src/img/**/*.min.*'])
-    .pipe(image())
-    .pipe(rename({
-      //extname: '.min.js',
-      suffix: '.min'
+    .pipe(newer('image/'))
+    .pipe(image([
+      image.gifsicle({interlaced: true}),
+      image.jpegtran({progressive: true}),
+      image.optipng({optimizationLevel: 5}),
+      image.svgo({
+        plugins: [
+          { removeViewBox: true },
+          { cleanupIDs: false }
+        ]
+      })
+    ]))
+    .pipe(size({
+      showFiles: true
     }))
     .pipe(dest('./docs/img'))
-    .pipe(dest('./src/img'))
-    .pipe(size());
+    .pipe(dest('./src/img'));
 }
 
 function minifyJs() {
@@ -58,23 +66,40 @@ function minifyJs() {
     .pipe(babel({
       presets: ['@babel/env']
     }))
-    //.pipe(gulp.src('./src/vendor/*.js'))
-    //.pipe(gulp.dest('./docs/js'))
     .pipe(uglify())
     .pipe(rename({
       suffix: '.min'
     }))
     .pipe(dest('./docs/js'))
-    .pipe(dest('./src/js'))
 }
 
 function minifyCss() {
-  return src(['./src/css/**/*.css'])
+  return src(['./src/scss/*.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+        fiber           : Fiber,
+        imagePath       : '/images/',
+        precision       : 3,
+        errLogToConsole : true
+      }).on('error', sass.logError))
+      .pipe(autoprefixer())
+      .pipe(csso())
+      .pipe(rename({
+        suffix: '.min'
+      }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(size({
+      showFiles: true
+    }))
     .pipe(dest('./docs/css'))
 }
 
 function minifyHtml() {
   return src('./src/**/*.html')
+    .pipe(htmlReplace({
+      'css': 'css/main.min.css',
+      'js': 'js/index.min.js'
+    }))
     .pipe(htmlmin({
       collapseWhitespace: true,
       removeComments: true
@@ -90,6 +115,7 @@ function fontCopy() {
 function cleanDist() {
   return src('./docs', { read: false })
     .pipe(clean());
+
 }
 
 exports.style = style;
@@ -104,103 +130,4 @@ exports.default = series(cleanDist, parallel(
     minifyJs, parallel(
       minifyCss, parallel(minifyHtml, fontCopy))
   )
-=======
-const gulp = require('gulp');
-const {series, parallel, src, dest} = require('gulp');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
-const image = require('gulp-imagemin');
-const babel = require('gulp-babel');
-const uglify = require('gulp-uglify');
-const rename = require('gulp-rename');
-const autoprefixer = require('gulp-autoprefixer');
-const csso = require('gulp-csso');
-const htmlmin = require('gulp-htmlmin');
-const clean = require('gulp-clean');
-
-function style() {
-  // 1. where is my sss file
-  return src('./src/scss/**/*.scss')
-    // 2. pass that file through compiler
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer())
-    // Minify css
-    .pipe(csso())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    // 3. where do i save the complete css
-    .pipe(dest('./src/css'))
-  // 4. stream changes to all browser
-    .pipe(browserSync.stream());
-}
-
-function watch() {
-  browserSync.init({
-    server: {
-      baseDir: './src'
-    }
-  });
-  gulp.watch('./src/scss/**/*.scss', style);
-  gulp.watch('./src/**/*.html').on('change', browserSync.reload);
-  gulp.watch('./src/js/**/*.js').on('change', browserSync.reload);
-}
-
-function minifyImage() {
-  return src(['./src/img/*', '!./src/img/**/*.min.*'])
-    .pipe(image())
-    .pipe(dest('./docs/img'))
-    .pipe(dest('./src/img'))
-}
-
-function minifyJs() {
-  return src(['./src/js/**/*.js', '!./src/js/**/*.min.js'])
-    .pipe(babel({
-      presets: ['@babel/env']
-    }))
-    .pipe(uglify())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(dest('./docs/js'))
-    .pipe(dest('./src/js'))
-}
-
-function minifyCss() {
-  return src(['./src/css/**/*.css'])
-    .pipe(dest('./docs/css'))
-}
-
-function minifyHtml() {
-  return src('./src/**/*.html')
-    .pipe(htmlmin({
-      collapseWhitespace: true,
-      removeComments: true
-    }))
-    .pipe(dest('./docs'))
-}
-
-function fontCopy() {
-  return src('./src/font/*')
-    .pipe(dest('./docs/font'));
-}
-
-function cleanDist() {
-  return src('./docs', { read: false })
-    .pipe(clean());
-}
-
-exports.style = style;
-exports.watch = watch;
-exports.minifyImage = minifyImage;
-exports.minifyJs = minifyJs;
-exports.minifyCss = minifyCss;
-exports.minifyHtml = minifyHtml;
-exports.cleanDist = cleanDist;
-exports.default = series(cleanDist, parallel(
-  minifyImage, parallel(
-    minifyJs, parallel(
-      minifyCss, parallel(minifyHtml, fontCopy))
-  )
->>>>>>> v0.0.0: setup enviroment
 ));
